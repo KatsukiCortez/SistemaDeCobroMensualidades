22d545e3a1f196642121954157b0c82bcbd6a9e5
JacuKingu <71521139@continental.edu.pe>
2023-06-21 15:00:46 -0500

    modificaciones a control de Matricula.fxml

--- SistemaCobroMensualidad/src/main/java/com/sistemacobromensualidad/ControlMatricula.java
+++ SistemaCobroMensualidad/src/main/java/com/sistemacobromensualidad/ControlMatricula.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.sistemacobromensualidad;
 
-/**
- *
- * @author burro
- */
+/*import javafx.faxml.FXML;
+import javafx.scente.control.TextField;
+import com.sistemacobromensualidad.Student;*/
+
 public class ControlMatricula {
     
 }
